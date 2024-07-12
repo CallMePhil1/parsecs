c8153884d77f21ed5709037ad4bf7463f98a92c6
CallMePhil <callmephil@github.com>
2024-07-11 22:58:27 -0400

    Created version generator for jar file names

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,36 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun generateVersion(): String {
+    val regex = Regex("${archivesName.get()}-(\\d+)\\.(\\d+)\\.(\\d+).(\\d+)\\.jar")
+    val libsDir = layout.buildDirectory.dir("libs").get()
+    val versionBase = LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY.MM.dd"))
+    val jarPrefix = "${archivesName.get()}-$versionBase"
+
+    println("Getting version name from dir ${libsDir.asFile.absolutePath}")
+    println("Archive name ${archivesName.get()}")
+    println("Prefix $jarPrefix")
+    println("Regex pattern: ${regex.pattern}")
+
+    val highestNumber = libsDir.asFileTree.files
+        .map { regex.find(it.name) }
+        .map { it?.groups?.get(4)?.value?.toInt() ?: 0 }
+        .maxBy { it }
+
+    println("Highest version number is $highestNumber")
+
+    return "${versionBase}.${highestNumber + 1}"
+}
 
 plugins {
     kotlin("jvm")
     //id("com.google.devtools.ksp")
 }
 
+archivesName = "parsec-core"
 group = "com.github.callmephil"
-version = "0.0.7"
+version = generateVersion()
 
 repositories {
     mavenCentral()
@@ -19,10 +43,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(kotlin("test"))
     api(kotlin("reflect"))
-}
-
-tasks.jar {
-    archivesName = "parsecs-core"
 }
 
 tasks.test {
