6efeff792c3f03026ac41c7f8c7eecf61e45b25f
CallMePhil <callmephil@github.com>
2025-01-04 13:11:49 -0500

    Add empty const for EntityRef

--- core/src/main/kotlin/com/github/callmephil1/parsecs/ecs/entity/EntityRef.kt
+++ core/src/main/kotlin/com/github/callmephil1/parsecs/ecs/entity/EntityRef.kt
@@ -4,4 +4,8 @@ class EntityRef(val entity: Entity) {
     val version: UInt = entity.version
 
     val isAlive get() = version == entity.version
+
+    companion object {
+        val EMPTY = EntityRef(Entity()).apply { entity.version = 1u }
+    }
 }
